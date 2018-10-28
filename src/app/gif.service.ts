ebacca3ce731f710fc60f83c44f28cb07f9a1cca
Cory Stone <corystone@gmail.com>
2018-10-27 19:47:16 -0500

    Hardcode external http call

--- src/app/gif.service.ts
+++ src/app/gif.service.ts
@@ -1,19 +1,42 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Gif } from './gif';
-import { GIFS } from './mock-gifs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifService {
 
+  private endpoint = 'http://45.32.194.17:9999/';
+
   getGifs(): Observable<Gif[]> {
-    // TODO: send the message _after_ fetching the gifs
-    this.messageService.add('GifService: fetched gifs');
-    return of(GIFS);
+    const search = this.endpoint + '?q=cat';
+    return this.http.get<Gif[]>(search).pipe(
+      tap(gifs => this.log('fetched gifs')),
+      catchError(this.handleError('getGifs', []))
+    );
   }
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
